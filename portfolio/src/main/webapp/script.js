95ce43346348f75803fc3e22753e874bd92c55f6
Connor Brett <connorbrett.18@gmail.com>
2020-06-08 19:19:01 +0000

    map library added

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -46,6 +46,12 @@ function deleteComments() {
   }).then(response => getComments());
 }
 
+function createMap() {
+  const map = new google.maps.Map(
+    document.getElementById('map'),
+    {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
+
 class slideShower {
   constructor() {
     this.currentIndex = 1;
